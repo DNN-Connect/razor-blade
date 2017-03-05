1985944e1cd27c0a84ea8f39960949fa1e1bfc55
iJungleboy <daniel.mettler@2sic.com>
2017-03-05 18:01:53 +0100

    first draft with Blades and test-project

---
+++ Razor Blades Tests/Blades_IsNoE.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using static Connect.Razor.Blades;
+
+
+namespace Razor_Blades_Tests
+{
+    [TestClass]
+    public class Blades_IsNoE
+    {
+        //[TestMethod]
+        //public void Test_IsNoE()
+        //{
+        //    string x = null;
+        //    Assert.IsTrue(IsNoE(x), "null should be empty");
+        //    Assert.IsTrue(IsNoE(""), "empty string should be empty");
+        //    Assert.IsFalse(IsNoE(" "), "space shouldn't be empty");
+        //    Assert.IsFalse(IsNoE("xyz"), "text shouldn't be empty");
+
+        //    // objects tests
+        //    object z = null;
+        //    Assert.IsTrue(IsNoE(z), "null object should be true");
+
+        //    string y = null;
+        //    object stringWithStrangeType = y;
+        //    Assert.IsTrue(IsNoE(y), "null-string should work too");
+
+        //}
+
+
+
+        [TestMethod]
+        public void Test_HasText()
+        {
+            Assert.IsFalse(HasText(null), "null should be no text");
+            Assert.IsFalse(HasText(""), "empty should be no text");
+            Assert.IsFalse(HasText("   "), "spaces should be no text");
+            Assert.IsFalse(HasText("			"), "tabs should be no text");
+            Assert.IsTrue(HasText("xyz"), "text shouldn't be null or WS");
+        }
+
+        [TestMethod]
+        public void Test_Fallback()
+        {
+            var val1Ok = "value 1";
+            string val1Null = null;
+            string val1Empty = "";
+            string val1spaces = "   ";
+            string fallback = "fallback!";
+
+            Assert.AreEqual(Fallback(val1Ok, fallback), val1Ok, "should be same");
+            Assert.AreEqual(Fallback(val1Null, fallback), fallback, "should be fallback");
+            Assert.AreEqual(Fallback(val1Empty, fallback), fallback, "should be fallback");
+            Assert.AreEqual(Fallback(val1spaces, fallback), fallback, "should be fallback");
+
+        }
+    }
+}
---
+++ Razor Blades Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Razor Blades Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Razor Blades Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("b163aa4d-8149-4a08-b79e-82b64ed940ee")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
