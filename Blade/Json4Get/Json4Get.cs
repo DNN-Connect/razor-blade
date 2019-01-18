ff943c99c4000410e6996bf789c90265a20dbc91
iJungleboy <daniel.mettler@2sic.com>
2019-01-18 18:15:09 +0100

    wip experimenting with json4get

---
+++ Blade/Json4Get/Json4Get.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Text;
+
+namespace Connect.Razor.Json4Get
+{
+    public static class Json4Get
+    {
+        public static char[] EncodeCharsOutside = {'{', '}', '"'};
+        public static char[] EncodeReplacements = { '(', ')', '\'' };
+        public const char EncodeCharInside = '\'';
+        public const string EncodeInsideReplacement = "''";
+        public const char EncodingSwitchModes = '"';
+        public const char EncodingSwitchBack = '\'';
+        public const char EncodingEscapeChar = '\\';
+
+
+        public static string Encode(string original)
+        {
+            var builder = new StringBuilder();
+            var outsideOfQuotes = true;
+            var prevChar = ' ';
+            foreach (var currentChar in original)
+            {
+                if (outsideOfQuotes)
+                {
+                    var index = Array.IndexOf(EncodeCharsOutside, currentChar);
+                    if (index == -1)
+                        builder.Append(currentChar);
+                    else
+                    {
+                        builder.Append(EncodeReplacements[index]);
+                        if (currentChar == EncodingSwitchModes)
+                            // ReSharper disable once ConditionIsAlwaysTrueOrFalse
+                            outsideOfQuotes = !outsideOfQuotes;
+                    }
+                }
+                else
+                {
+                    if (currentChar == EncodeCharInside)
+                        builder.Append(EncodeInsideReplacement);
+                    else if (currentChar == EncodingSwitchModes && prevChar != EncodingEscapeChar)
+                    {
+                        builder.Append(EncodingSwitchBack);
+                        // ReSharper disable once ConditionIsAlwaysTrueOrFalse
+                        outsideOfQuotes = !outsideOfQuotes;
+                    }
+                    else
+                        builder.Append(currentChar);
+                }
+                prevChar = currentChar;
+            }
+            return builder.ToString();
+        }
+
+        public static string Decode(string original)
+        {
+            var builder = new StringBuilder();
+            var outsideOfQuotes = true;
+            var prevChar = ' ';
+            foreach (var currentChar in original)
+            {
+                if (outsideOfQuotes)
+                {
+                    var index = Array.IndexOf(EncodeReplacements, currentChar);
+                    if (index == -1)
+                        builder.Append(currentChar);
+                    else
+                    {
+                        builder.Append(EncodeCharsOutside[index]);
+                        if (currentChar == EncodingSwitchBack)
+                            outsideOfQuotes = !outsideOfQuotes;
+                    }
+                }
+                else
+                {
+                    
+                }
+                prevChar = currentChar;
+            }
+            return builder.ToString();
+        }
+    }
+}
---
+++ Razor Blades Tests/TestJson4Get.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections.Generic;
+using Connect.Razor.Json4Get;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Newtonsoft.Json;
+
+namespace Razor_Blades_Tests
+{
+    [TestClass]
+    public class TestJson4Get
+    {
+        public static Dictionary<string, OneValue> SimpleValues = new Dictionary<string, OneValue>
+        {
+            {"('Val':'simple')", new OneValue("simple")},
+            {"('Val':8)", new OneValue(8)},
+            {"('Val':'single quote ''')", new OneValue("single quote '")},
+            {"('Val':'multi ''single quote''')", new OneValue("multi 'single quote'")},
+            {"('Val':'multi single ''''')", new OneValue("multi single ''") },
+            {"('Val':'double \\\"')", new OneValue("double \"") },
+        };
+
+
+        [TestMethod]
+        public void SingleValues()
+        {
+            foreach (var test in SimpleValues)
+            {
+                var json = JsonConvert.SerializeObject(test.Value);
+                var json4get = Json4Get.Encode(json);
+                Assert.AreEqual(test.Key, json4get, $"trouble with:{test.Value.Val}");
+            }
+        }
+
+        [TestMethod]
+        public void EncodeBasic()
+        {
+            var test = new
+            {
+                Key = "something",
+                Value = "something else",
+                SomeArray = new[] {"a string", "another string"},
+                IntArr = new[] {7, 8, 8}
+            };
+            var expected = "('Key':'something','Value':'something else','SomeArray':['a string','another string'],'IntArr':[7,8,8])";
+            var json = JsonConvert.SerializeObject(test);
+            var result = Json4Get.Encode(json);
+            Assert.AreEqual(expected, result);
+        }
+
+        public class OneValue
+        {
+            public object Val;
+            public OneValue(object value) => Val = value;
+        }
+    }
+}
