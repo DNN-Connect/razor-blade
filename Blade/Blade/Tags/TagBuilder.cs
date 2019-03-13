dae3d7a0e368833b43ccac864bbe6e1fc5cbd538
iJungleboy <daniel.mettler@2sic.com>
2019-03-13 18:56:56 +0100

    wip working on icons...

---
+++ Blade/Blade/Tags/TagBuilder.cs
@@ -0,0 +1,18 @@
+﻿namespace Connect.Razor.Blade
+{
+    /// <summary>
+    /// WIP: create basic tools to generate atttributes and tags here, probably also move "Wrap" to this
+    /// </summary>
+    partial class Tags
+    {
+        internal static string Attribute(string value)
+        {
+            return "";
+        }
+
+        internal static string Attribute(string name, string value)
+        {
+            return "";
+        }
+    }
+}
---
+++ Connect.Razor.Dnn/Dnn/DnnPage_Icons.cs
@@ -0,0 +1,45 @@
+﻿using System.Web.UI;
+using System.Web.UI.HtmlControls;
+
+namespace Connect.Razor.Dnn
+{
+    public partial class DnnHtmlPage 
+    {
+        public void AddToHead(string tag)
+        {
+            try
+            {
+                if (string.IsNullOrWhiteSpace(tag)) return;
+                var control = new LiteralControl(tag);
+                Page?.Header.Controls.Add(control);
+            }
+            catch {  /* ignore */ }
+        }
+
+        public void AddMeta(string name, string content)
+            => AddToHead($"<meta name=\'{name}\' content=\'{Attribute(content)}\' /> ");
+
+        public void AddOpenGraph(string property, string content)
+            => AddToHead($"<meta property=\'{property}\' content=\'{Attribute(content)}\' /> ");
+
+        public void AddJsonLd(string jsonString)
+            => AddToHead($"<script type=\"application/ld+json\">{jsonString}</script>");
+
+        public void AddJsonLd(object jsonObject)
+        {
+            var str = new System.Web.Script.Serialization.JavaScriptSerializer().Serialize(jsonObject);
+            AddJsonLd(str);
+        }
+
+        private void EnsureFieldVisibleAndSetValueAgain(string id, string value)
+        {
+            if (!(Page?.FindControl(id) is HtmlMeta metaTag)) return;
+            metaTag.Visible = true;
+            // todo: 2rm check why we are doing this - feels like we're setting things 2x
+            metaTag.Content = Attribute(value);
+        }
+
+
+
+    }
+}
